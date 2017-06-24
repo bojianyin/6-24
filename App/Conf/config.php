45b0813b3b20c021d47d15a161d33059fbc00d2b
lbo <2316744840@qq.com>
2017-06-24 18:49:50 +0800

    Signed-off-by: lbo <2316744840@qq.com> 6/24

---
+++ App/Conf/config.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	/**
+	 * 应用分组
+	 */
+	"APP_GROUP_LIST"=>"Index,Admin"
+	,"DEFAULT_GROUP"=>"Index"
+	//数据库连接
+	,"DB_HOST"=>"127.0.0.1"
+	,"DB_USER"=>"root"
+	,"DB_PWD"=>"root"
+	,"DB_NAME"=>"usersave"
+	,"DB_PREFIX"=>"lx_"    //表前缀
+	//点语法默认解析
+	,"TMPL_VAR_IDENTIFY"=>"array"
+	//默认过滤函数
+	,"DEFAULT_FILTER"=>"htmlspecialchars"
+);
+?>
---
+++ App/Lib/Action/Admin/IndexAction.class.php
@@ -0,0 +1,13 @@
+<?php
+	/**
+	* 后台index入口
+	*/
+	class IndexAction extends Action
+	{
+		Public function Index(){
+			echo "这里是admin入口";
+		}
+		
+	}
+	
+?>
